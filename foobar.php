08af04b068ed3b037ea669bfd84c92f131dcb919
Thumrith R <thumrithr@gmail.com>
2021-06-22 22:45:32 +1000

    Adding Header Comments and Generating numbers array
    Adding Header comments and Generating numbers array using the range functionality

---
+++ foobar.php
@@ -0,0 +1,18 @@
+<?php
+/**
+* Name : Catalyst-PHP-Developer-Task-02
+* Date : 22-06-2021
+* Dev : Chinzy Ranasinghe
+* Question : Create a PHP script that is executed form the command line. The script should:
+			 • Output the numbers from 1 to 100
+			 • Where the number is divisible by three (3) output the word “foo”
+			 • Where the number is divisible by five (5) output the word “bar”
+			 • Where the number is divisible by three (3) and (5) output the word “foobar”
+			 • Only be a single PHP file
+* Output(ex) : 1, 2, foo, 4, bar, foo, 7, 8, foo, bar, 11, foo, 13, 14, foobar...  
+**/
+
+// generating an array from 1 to 100 using range function
+$array_output = range(1,100);
+
+?>
